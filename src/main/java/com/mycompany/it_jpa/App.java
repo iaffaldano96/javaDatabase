bc746a1c1e40ac91a8e1dedade55a0620dbe4b45
tss <tss@debian-tss24>
2017-01-16 12:24:49 +0100

    connect db

---
+++ src/main/java/com/mycompany/it_jpa/App.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.it_jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author tss
+ */
+public class App {
+    
+    public static void main(String[] args) {
+        
+        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu"); //collego al db
+        EntityManager em = emf.createEntityManager();  // em per fare le operazione sul db 
+        
+        Messaggio msg=new Messaggio("Pranzo...");
+
+        em.getTransaction().begin();
+        em.merge(msg);
+        em.getTransaction().commit();
+    }
+}
---
+++ src/main/java/com/mycompany/it_jpa/Messaggio.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.it_jpa;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author tss
+ */
+@Entity
+@Table (name="messaggio")
+public class Messaggio implements Serializable{
+    
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    private String testo;
+
+    public Messaggio() {
+    }
+
+    public Messaggio( String testo) {        
+        this.testo = testo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTesto() {
+        return testo;
+    }
+
+    public void setTesto(String testo) {
+        this.testo = testo;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Messaggio other = (Messaggio) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Messaggio{" + "id=" + id + ", testo=" + testo + '}';
+    }
+    
+    
+}
